9025db46a102a8dfd06b271ed2896103ec41c35f
Abel P <abelpinales97@gmail.com>
2024-11-08 00:54:44 -0500

    Stopped at step Five of add Images to your android App

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(message = "Happy Birthday Claire",
+                    GreetingImage(message = "Happy Birthday Claire",
                                  from = "From Abel" ,
                                  modifier = Modifier.padding(8.dp))
                 }
@@ -50,7 +53,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
         ) {
         Text(
             text = message,
-            fontSize = 100.sp,
+            fontSize = 80.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center
         )
@@ -64,12 +67,31 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     }
 }
 
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image  = painterResource(R.drawable.androidparty)
+    Box(Modifier) {
+        Image(
+            painter = image,
+            null
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-
-        GreetingText(message = "Happy Birthday Abel!", from = "From Claire")
-
+        GreetingImage(message = "Happy Birthday A", from = "A Real fun guy")
     }
 }
